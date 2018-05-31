7121567eb935af1703b9eaf8703290a9c38a1dba
Brandon Villa <bornofos@gmail.com>
2018-05-31 12:13:32 -0500

    MuiThemeProvider component added

--- src/App.js
+++ src/App.js
@@ -4,14 +4,19 @@ import Dashboard from './containers/Dashboard';
 import Login from './containers/Login';
 import Signin from './containers/Signin';
 
+import { MuiThemeProvider } from '@material-ui/core/styles';
+import theme from './material-theme';
+
 class App extends Component {
   render() {  
     return (
-      <Switch>
-        <Route path="/dashboard" component={Dashboard} />
-        <Route path="/login" component={Login} />
-        <Route path="/signin" component={Signin} />
-      </Switch>
+      <MuiThemeProvider theme={theme}>
+        <Switch>
+          <Route path="/dashboard" component={Dashboard} />
+          <Route path="/login" component={Login} />
+          <Route path="/signin" component={Signin} />
+        </Switch>
+      </MuiThemeProvider>
     );
   }
 }
