e45dad403f9ad4dbd098e8ccd7c4814b71e28ece
Brandon Villa <bornofos@gmail.com>
2018-05-31 11:19:17 -0500

    First routes added to main component (App.js)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
+import Dashboard from './layouts/Dashboard/DashboardContent';
+import Login from './layouts/Session/LoginContent';
+import Signin from './layouts/Session/SigninContent';
 
 class App extends Component {
-  render() {
+  render() {  
     return (
-      <h1>Hello world</h1>
+      <Switch>
+        <Route path="/dashboard" component={Dashboard} />
+        <Route path="/login" component={Login} />
+        <Route path="/signin" component={Signin} />
+      </Switch>
     );
   }
 }
