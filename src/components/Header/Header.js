7c6a91c9489e9437fc0de2e98b87302588565981
Brandon Villa <bornofos@gmail.com>
2018-05-31 12:59:44 -0500

    Header component created

---
+++ src/components/Header/Header.js
@@ -0,0 +1,46 @@
+import React, { Component, PropTypes } from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import Typography from '@material-ui/core/Typography';
+import { styles as classes } from './styles';
+
+class Header extends Component {
+    constructor(props) {
+        super(props);
+        this.clearLocalStorage = this.clearLocalStorage.bind(this);
+    }
+
+    clearLocalStorage() {
+        localStorage.clear();
+    }
+
+    render() {
+        const { styles, handleChangeRequestNavDrawer } = this.props;
+
+        return (
+            <div style={classes.root}>
+                <AppBar style={styles} position="static">
+                    <Toolbar>
+                        <IconButton
+                            style={classes.menuButton}
+                            color="inherit"
+                            aria-label="Menu"
+                            onClick={handleChangeRequestNavDrawer}
+                        >
+                            <MenuIcon />
+                        </IconButton>
+                        <Typography variant="title" color="inherit" style={classes.flex}>
+                            Title
+                        </Typography>
+                        <Button color="inherit">Login</Button>
+                    </Toolbar>
+                </AppBar>
+            </div>
+        )
+    }
+}
+
+export default Header;
---
+++ src/components/Header/styles.js
@@ -0,0 +1,12 @@
+export const styles = {
+    root: {
+        flexGrow: 1,
+    },
+    flex: {
+        flex: 1,
+    },
+    menuButton: {
+        marginLeft: -12,
+        marginRight: 20,
+    },
+};
