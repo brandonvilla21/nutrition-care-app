3594039045c161de2c40f2497f28c9b2bf2f4fb1
Brandon Villa <bornofos@gmail.com>
2018-05-31 11:48:30 -0500

    CssBaseline component added

--- src/index.js
+++ src/index.js
@@ -3,13 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import CssBaseline from '@material-ui/core/CssBaseline';
 
 import { BrowserRouter } from 'react-router-dom';
 
 const app = (
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>
+    <div>
+        <CssBaseline />
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </div>
 );
 
 ReactDOM.render( app, document.getElementById('root') );
