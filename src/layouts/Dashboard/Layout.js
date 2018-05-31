c0e0d1c38f38346a9b62014ed8eb899b92e2e2d9
Brandon Villa <bornofos@gmail.com>
2018-05-31 12:59:55 -0500

    Header and state added

--- src/layouts/Dashboard/Layout.js
+++ src/layouts/Dashboard/Layout.js
@@ -1,12 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Header from '../../components/Header/';
 
-const Layout = (props) => {
-    return (
-        <div>
-            <h3>This is the Dashboard layout</h3>
-            {props.children}
-        </div>
-    );
+class Layout extends Component {
+    constructor(props) {
+        super(props);
+        
+        this.state = {
+            navDrawerOpen: false
+        };
+
+        this.handleChangeRequestNavDrawer = this.handleChangeRequestNavDrawer.bind(this);
+    }
+    
+    handleChangeRequestNavDrawer() {
+        this.setState({
+          navDrawerOpen: !this.state.navDrawerOpen
+        });
+    }
+    
+    render() {
+        let { navDrawerOpen } = this.state;
+        const paddingLeftDrawerOpen = 236;
+    
+        const styles = {
+          header: {
+            paddingLeft: navDrawerOpen ? paddingLeftDrawerOpen : 0
+          }
+        };
+        console.log(styles.header);
+        return (
+            <div>
+                <Header
+                    styles={styles.header}
+                    handleChangeRequestNavDrawer={this.handleChangeRequestNavDrawer} />
+                {this.props.children}
+            </div>
+        );
+    }
 }
 
 export default Layout;
