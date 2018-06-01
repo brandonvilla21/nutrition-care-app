9b76266321b2860c166d43be19181f8a3476d36a
Brandon Villa <bornofos@gmail.com>
2018-05-31 20:15:04 -0500

    theme re-configured

--- src/material-theme.js
+++ src/material-theme.js
@@ -1,22 +1,21 @@
 import { createMuiTheme } from '@material-ui/core/styles';
+import pink from '@material-ui/core/colors/pink';
+import red from '@material-ui/core/colors/red';
 import blue from '@material-ui/core/colors/blue';
-import white from '@material-ui/core/colors/blue';
-
 
 const theme = createMuiTheme({
   palette: {
+    primary: blue,
+    secondary: pink,
+    error: red,
+    // Used by `getContrastText()` to maximize the contrast between the background and
+    // the text.
+    contrastThreshold: 3,
+    // Used to shift a color's luminance by approximately
+    // two indexes within its tonal palette.
+    // E.g., shift from Red 500 to Red 300 or Red 700.
+    tonalOffset: 0.2,
   },
-  appBar: {
-    height: 57,
-    color: blue[500]
-  },
-  drawer: {
-    width: 230,
-    color: white
-  },
-  raisedButton: {
-    primaryColor: blue[500],
-  }
 });
 
 
