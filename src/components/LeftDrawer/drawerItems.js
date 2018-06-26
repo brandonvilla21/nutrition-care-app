4b04baca4b4e1bd4883b2acebb70e4c97fc94e6f
Brandon Villa <bornofos@gmail.com>
2018-06-26 13:10:22 -0500

    Links added to ListItems

--- src/components/LeftDrawer/drawerItems.js
+++ src/components/LeftDrawer/drawerItems.js
@@ -3,28 +3,28 @@ import { ListItem, ListItemIcon, ListItemText, Divider } from "@material-ui/core
 import PermIdentity from '@material-ui/icons/PermIdentity';
 import FitnessCenter from '@material-ui/icons/FitnessCenter';
 import Restaurant from '@material-ui/icons/Restaurant';
+import { Link } from 'react-router-dom';
 
 export const drawerItems = (
     <div>
-        <ListItem button>
+        <ListItem button component={Link} to="/routine" >
             <ListItemIcon>
                 <FitnessCenter />
             </ListItemIcon>
             <ListItemText primary="Rutinas" />
         </ListItem>
-        <ListItem button>
+        <ListItem button component={Link} to="/diet">
             <ListItemIcon>
                 <Restaurant />
             </ListItemIcon>
             <ListItemText primary="Dietas" />
         </ListItem>
         <Divider />
-        <ListItem button>
+        <ListItem button component={Link} to="/my-info">
             <ListItemIcon>
                 <PermIdentity />
             </ListItemIcon>
             <ListItemText primary="Mi información" />
         </ListItem>
-        
     </div>
 );
