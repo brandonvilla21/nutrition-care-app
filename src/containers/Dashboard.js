a1d66374b8679c023ac4a108853f28637f3b3aa3
Brandon Villa <bornofos@gmail.com>
2018-06-26 12:44:41 -0500

    Change of logic in routes

--- src/containers/Dashboard.js
+++ src/containers/Dashboard.js
@@ -5,14 +5,30 @@ import Layout from '../layouts/Dashboard';
 const DashboardContent = (props) => {
     return (
         <Layout>
-            <h5>Dashboard Content (Instead of this, should be the routes)</h5>
+            <h5>Dashboard Content</h5>
+            {/* 
+                Change of logic:
+                Routes shouldn't be here
+                Why?: because this component doesn't have child routes
+                However, the route "/routines" DOES have child routes
+                    that could be: "/routines/create", "/routines/edit", "/routines/:id", etc.
+                    same happens with: "/diets":  "/diets/create", etc.
+
+                New solution:
+                What will it be in this component?: Just the content for the dashboard
+                Where will the routes be now?: In App component, along with:
+                    "/"
+                    "login"
+                    "signin"
+                    And now (names are still tentative):
+                    "/routines"
+                    "/diets"
+                    "/my-info"
+             */}
             
-            {/* Something like this */}
-            {/* <Switch>
-                <Route path="/checkout" component={Checkout} />
-                <Route path="/orders" component={Orders} />
-                <Route path="/" exact component={BurgerBuilder} />
-            </Switch> */}
+            
+            
+            {/* Dashboard content goes here */}
         </Layout>
     );
 }
