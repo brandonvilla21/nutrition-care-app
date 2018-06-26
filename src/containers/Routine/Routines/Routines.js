aa5464943eba1635c3918c65dd72401cef86d7c0
Brandon Villa <bornofos@gmail.com>
2018-06-26 16:13:39 -0500

    Initial code for Routines where all routines should be displayed

---
+++ src/containers/Routine/Routines/Routines.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import AddButton from '../../../components/AddButton';
+
+const Routines = ({ classes }) => {
+    return (
+        <div>
+            <h2>This sould show all routines</h2>
+            <AddButton />
+        </div>
+    );
+};
+
+
+export default Routines;
