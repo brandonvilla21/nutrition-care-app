51709fb689748ddff9d66ff4defeeb280b857291
Brandon Villa <bornofos@gmail.com>
2018-06-28 09:56:23 -0500

    first part of logic

--- src/containers/Routine/CreateRoutine/CreateRoutine.js
+++ src/containers/Routine/CreateRoutine/CreateRoutine.js
@@ -3,17 +3,37 @@ import PageBase from '../../../components/PageBase/PageBase';
 import TabsRoutine from '../../../components/Routine/TabsRoutine/TabsRoutine';
 
 class CreateRoutine extends Component {
-    render() {
-      return (
-        <div>
-          <PageBase>
-            <div>
-              <TabsRoutine />
-            </div>
-          </PageBase>
-        </div>
-      )
-    }
+  constructor() {
+    super();
+    this.state = {
+      description: '',
+    };
+    this.handleInput = this.handleInput.bind(this);
+  }
+  handleInput( event ) {
+    const name = event.target.name;
+    const value = event.target.value;
+
+    this.setState({
+        [name]: value
+    });
+  }
+  render() {
+    const { description } = this.state;
+    
+    return (
+      <div>
+        <PageBase>
+          <div>
+            <TabsRoutine
+              description={description}
+              handleInput={this.handleInput}
+            />
+          </div>
+        </PageBase>
+      </div>
+    )
+  }
 }
 
 export default CreateRoutine;
