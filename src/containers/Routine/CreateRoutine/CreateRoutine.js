0ba5d0606fd72e30005678bfa7c5b5f2eaca12a4
Brandon Villa <bornofos@gmail.com>
2018-06-28 15:47:43 -0500

    All logic of state removed and now usign redux

--- src/containers/Routine/CreateRoutine/CreateRoutine.js
+++ src/containers/Routine/CreateRoutine/CreateRoutine.js
@@ -3,32 +3,13 @@ import PageBase from '../../../components/PageBase/PageBase';
 import TabsRoutine from '../../../components/Routine/TabsRoutine/TabsRoutine';
 
 class CreateRoutine extends Component {
-  constructor() {
-    super();
-    this.state = {
-      description: '',
-    };
-    this.handleInput = this.handleInput.bind(this);
-  }
-  handleInput( event ) {
-    const name = event.target.name;
-    const value = event.target.value;
 
-    this.setState({
-        [name]: value
-    });
-  }
   render() {
-    const { description } = this.state;
-    
     return (
       <div>
         <PageBase>
           <div>
-            <TabsRoutine
-              description={description}
-              handleInput={this.handleInput}
-            />
+            <TabsRoutine />
           </div>
         </PageBase>
       </div>
