e7797cce5d30b8b6c5e49eaed06cc34dd7d6a446
Brandon Villa <bornofos@gmail.com>
2018-06-28 09:55:45 -0500

    Removed comments

--- src/containers/Dashboard.js
+++ src/containers/Dashboard.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 import Layout from '../layouts/Dashboard';
 import Routine from './Routine/Routine';
 
@@ -7,34 +7,12 @@ const DashboardContent = ({ match }) => {
     return (
         <Layout>
             
-            {/* 
-                Change of logic:
-                Routes shouldn't be here
-                Why?: because this component doesn't have child routes
-                However, the route "/routines" DOES have child routes
-                    that could be: "/routines/create", "/routines/edit", "/routines/:id", etc.
-                    same happens with: "/diets":  "/diets/create", etc.
-
-                New solution:
-                What will it be in this component?: Just the content for the dashboard
-                Where will the routes be now?: In App component, along with:
-                    "/"
-                    "login"
-                    "signin"
-                    And now (names are still tentative):
-                    "/routines"
-                    "/diets"
-                    "/my-info"
-             */}
-            
-            
             <Route
                 exact
                 path={match.url}
                 render={() => <h5>Dashboard Content</h5>}
             />
             <Route path={`${match.url}/routine`} component={Routine}/>
-            {/* Dashboard content goes here */}
         </Layout>
     );
 }
