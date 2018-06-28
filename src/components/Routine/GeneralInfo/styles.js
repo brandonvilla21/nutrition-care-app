5119d2abd01032da043625f8ac04433c85e96f3b
Brandon Villa <bornofos@gmail.com>
2018-06-28 15:45:10 -0500

    styles file for GeneralInfo

---
+++ src/components/Routine/GeneralInfo/styles.js
@@ -0,0 +1,9 @@
+export const styles = theme => ({
+    buttonContainer: {
+      display: 'flex',
+      justifyContent: 'flex-end'
+    },
+    button: {
+        margin: theme.spacing.unit,
+    }
+});
