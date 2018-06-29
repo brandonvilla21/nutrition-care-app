1ae2c52bdfddfaf1cf3aaa21567b5a7644f230dd
Brandon Villa <bornofos@gmail.com>
2018-06-29 14:14:58 -0500

    padding readjusted

--- src/components/Routine/TabsRoutine/TabContainer.js
+++ src/components/Routine/TabsRoutine/TabContainer.js
@@ -4,7 +4,7 @@ import { Typography } from '@material-ui/core';
 
 const TabContainer = ({ children }) => {
     return (
-        <Typography component="div" style={{ padding: 24 }}>
+        <Typography component="div" style={{ padding: '24px 8px' }}>
             {children}
         </Typography>
     );
