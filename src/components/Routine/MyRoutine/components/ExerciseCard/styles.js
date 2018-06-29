edeee7fd3a7a5e407045b0e00823daf7227ef552
Brandon Villa <bornofos@gmail.com>
2018-06-29 14:14:43 -0500

    styles file for ExerciseCard component

---
+++ src/components/Routine/MyRoutine/components/ExerciseCard/styles.js
@@ -0,0 +1,28 @@
+import red from '@material-ui/core/colors/red';
+
+export const styles = theme => ({
+    card: {
+      maxWidth: 400,
+    },
+    media: {
+      height: 0,
+      paddingTop: '56.25%', // 16:9
+    },
+    actions: {
+      display: 'flex',
+    },
+    expand: {
+      transform: 'rotate(0deg)',
+      transition: theme.transitions.create('transform', {
+        duration: theme.transitions.duration.shortest,
+      }),
+      marginLeft: 'auto',
+    },
+    expandOpen: {
+      transform: 'rotate(180deg)',
+    },
+    avatar: {
+      backgroundColor: red[500],
+    },
+});
+  
