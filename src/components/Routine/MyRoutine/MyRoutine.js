a6ff1ac75acfdbd95f5038981b521810cd1ea3ec
Brandon Villa <bornofos@gmail.com>
2018-06-29 14:16:29 -0500

    Now it renders the RoutineDay components based on the days selectes for the user

--- src/components/Routine/MyRoutine/MyRoutine.js
+++ src/components/Routine/MyRoutine/MyRoutine.js
@@ -3,11 +3,20 @@ import PropTypes from 'prop-types';
 import { Button, withStyles } from '@material-ui/core';
 import { styles } from './styles';
 import SelectDay from './components/SelectDay/SelectDay';
+import { connect } from 'react-redux';
+import RoutineDay from './components/RoutineDay/RoutineDay';
 
-const MyRoutine = ({ classes, nextIndex, prevIndex }) => {
+const MyRoutine = ({ classes, nextIndex, prevIndex, days }) => {
     return (
         <div>
             <SelectDay />
+            <div className={classes.daysContainer}>
+                {
+                    days.map(( day, index ) => 
+                        <RoutineDay key={index}/>
+                    )
+                }
+            </div>
             <div className={classes.buttonsContainer}>
                 <Button
                     onClick={prevIndex}
@@ -25,10 +34,13 @@ const MyRoutine = ({ classes, nextIndex, prevIndex }) => {
         </div>
     );
 };
+const mapStateToProps = state => ({
+    days: state.days
+});
 
 MyRoutine.propTypes = {
     nextIndex: PropTypes.func,
     prevIndex: PropTypes.func,
 };
 
-export default withStyles(styles)(MyRoutine);
+export default connect(mapStateToProps)(withStyles(styles)(MyRoutine));
