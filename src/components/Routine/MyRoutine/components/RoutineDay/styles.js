c388023ae3e08a11c69f147678dd317d52f716d8
Brandon Villa <bornofos@gmail.com>
2018-06-29 14:16:02 -0500

    styles file added for RoutineDay component

---
+++ src/components/Routine/MyRoutine/components/RoutineDay/styles.js
@@ -0,0 +1,9 @@
+export const styles = theme => ({
+    root: {
+      width: '100%',
+    },
+    heading: {
+      fontSize: theme.typography.pxToRem(15),
+      fontWeight: theme.typography.fontWeightRegular,
+    },
+});
