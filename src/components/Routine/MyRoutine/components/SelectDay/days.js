e835ab558b9acfa6bc387a2a9be367b0c4a9ca69
Brandon Villa <bornofos@gmail.com>
2018-06-29 12:44:26 -0500

    days displayed in select

---
+++ src/components/Routine/MyRoutine/components/SelectDay/days.js
@@ -0,0 +1,9 @@
+export default [
+    { id: 0, name: 'Lunes', selected: false },
+    { id: 1, name: 'Martes', selected: false },
+    { id: 2, name: 'Miércoles', selected: false },
+    { id: 3, name: 'Jueves', selected: false },
+    { id: 4, name: 'Viernes', selected: false },
+    { id: 5, name: 'Sábado', selected: false },
+    { id: 6, name: 'Domingo', selected: false },
+];
