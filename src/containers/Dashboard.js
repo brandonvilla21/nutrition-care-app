41e5123b57c0d8f9dc80e9c8fc2bdf551bbbad82
Brandon Villa <bornofos@gmail.com>
2018-07-04 18:39:51 -0500

    Food route added

--- src/containers/Dashboard.js
+++ src/containers/Dashboard.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import Layout from '../layouts/Dashboard';
 import Routine from './Routine/Routine';
+import Food from './Food/Food';
 
 const DashboardContent = ({ match }) => {
     return (
@@ -13,6 +14,7 @@ const DashboardContent = ({ match }) => {
                 render={() => <h5>Dashboard Content</h5>}
             />
             <Route path={`${match.url}/routine`} component={Routine}/>
+            <Route path={`${match.url}/food`} component={Food}/>
         </Layout>
     );
 }
