14a08c97ff55ecea5cc3d4b72dd881512087f833
Brandon Villa <bornofos@gmail.com>
2018-07-09 13:29:55 -0500

    Card component to display in Signin component

---
+++ src/components/Signin/Card/index.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core';
+import Typography from '@material-ui/core/Typography';
+import styles from './styles';
+
+const Card = ({ classes, Icon, iconColor, title, body }) => {
+    return(
+        <div className={classes.container}>
+            <div>
+                <Icon style={{ fontSize: 36, color: iconColor }}/>
+            </div>
+            <div className={classes.textContent}>
+                <div className={classes.title}>
+                    <Typography variant="headline">
+                        {title}
+                    </Typography>
+                </div>
+                <div>
+                    <Typography className={classes.textBody} variant="caption">
+                        {body}
+                    </Typography>
+                </div>
+            </div>
+        </div>
+    )
+};
+
+Card.propTypes = {
+    classes: PropTypes.object,
+    Icon: PropTypes.func,
+    iconColor: PropTypes.string,
+    title: PropTypes.string,
+    body: PropTypes.string,
+}
+
+export default withStyles(styles)(Card);
---
+++ src/components/Signin/Card/styles.js
@@ -0,0 +1,18 @@
+export default () => ({
+    container: {
+        display: 'flex',
+        alignItems: 'stretch',
+        width: '400px',
+        padding: 8,
+    },
+    textContent: {
+        marginLeft: 8,
+    },
+    title: {
+        paddingTop: 4,
+        paddingBottom: 8
+    },
+    textBody: {
+        fontSize: 16
+    },
+});
