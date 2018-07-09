b5a8462fbcac7ce1884b2d56d1d0b90e31625d29
Brandon Villa <bornofos@gmail.com>
2018-07-09 12:07:23 -0500

    This component fetch real data from server now

--- src/containers/Food/Foods/Foods.js
+++ src/containers/Food/Foods/Foods.js
@@ -1,66 +1,61 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { withStyles, Table, TableHead, TableRow, TableCell, TableBody, Paper, Typography, Divider } from '@material-ui/core';
 import styles from './styles';
 import AddButton from '../../../components/AddButton';
+import axios from '../../../axios';
 
-const Foods = (props) => {
-    
-    let id = 0;
-    function createData(name, calories, fat, carbs, protein) {
-        id += 1;
-        return { id, name, calories, fat, carbs, protein };
+class Foods extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            food: []
+        }
+    }
+    componentDidMount() {
+        axios.get('/Food')
+            .then(res => this.setState({ food: res.data }))
+            .catch(err => err);
     }
 
-    const data = [
-        createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-        createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-        createData('Eclair', 262, 16.0, 24, 6.0),
-        createData('Cupcake', 305, 3.7, 67, 4.3),
-        createData('Gingerbread', 356, 16.0, 49, 3.9),
-        createData('Pan con chocolate', 356, 16.0, 49, 3.9),
-        createData('Huevitos', 356, 16.0, 49, 3.9),
-        createData('Mantecadas bimbo', 356, 16.0, 49, 3.9),
-        createData('Torta ahogada', 356, 16.0, 49, 3.9),
-        createData('Tostadas pepe', 356, 16.0, 49, 3.9),
-    ];
-
-    const { classes } = props;
-    
-    return (
-        <div>
-            <Typography className={classes.title} variant="headline">Alimentos</Typography>
-            <Divider/>
-            <Paper className={classes.root}>
-                <Table className={classes.table}>
-                    <TableHead>
-                        <TableRow>
-                            <TableCell>Dessert (100g serving)</TableCell>
-                            <TableCell numeric>Calories</TableCell>
-                            <TableCell numeric>Fat (g)</TableCell>
-                            <TableCell numeric>Carbs (g)</TableCell>
-                            <TableCell numeric>Protein (g)</TableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {data.map(n => {
-                            return (
-                            <TableRow key={n.id}>
-                                <TableCell component="th" scope="row">
-                                {n.name}
-                                </TableCell>
-                                <TableCell numeric>{n.calories}</TableCell>
-                                <TableCell numeric>{n.fat}</TableCell>
-                                <TableCell numeric>{n.carbs}</TableCell>
-                                <TableCell numeric>{n.protein}</TableCell>
+    render() {
+        const { classes } = this.props;
+        const { food } = this.state;
+        return (
+            <div>
+                <Typography className={classes.title} variant="headline">Alimentos</Typography>
+                <Divider/>
+                <Paper className={classes.root}>
+                    <Table className={classes.table}>
+                        <TableHead>
+                            <TableRow>
+                                <TableCell>Description</TableCell>
+                                <TableCell numeric>Calories</TableCell>
+                                <TableCell numeric>Fat (g)</TableCell>
+                                <TableCell numeric>Carbs (g)</TableCell>
+                                <TableCell numeric>Protein (g)</TableCell>
                             </TableRow>
-                            );
-                        })}
-                    </TableBody>
-                </Table>
-            </Paper>
-            <AddButton to="/dashboard/food/create"/>
-        </div>
-    );
+                        </TableHead>
+                        <TableBody>
+                            {
+                                food.map( f => 
+                                    <TableRow key={f.id}>
+                                        <TableCell component="th" scope="row">
+                                            {f.description}
+                                        </TableCell>
+                                        <TableCell numeric>{f.calories}</TableCell>
+                                        <TableCell numeric>{f.fats}</TableCell>
+                                        <TableCell numeric>{f.carbohydrates}</TableCell>
+                                        <TableCell numeric>{f.proteins}</TableCell>
+                                    </TableRow>
+                                )
+                            }
+                        </TableBody>
+                    </Table>
+                </Paper>
+                <AddButton to="/dashboard/food/create"/>
+            </div>
+        );
+    }
 }
 
 export default  withStyles(styles, { withTheme: true })(Foods);
