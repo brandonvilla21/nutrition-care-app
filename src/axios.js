d99f1fc44eaba556bcf26807fe0d8334dc7f07a4
Brandon Villa <bornofos@gmail.com>
2018-08-24 19:25:03 -0500

    axios interceptor added to add token header

--- src/axios.js
+++ src/axios.js
@@ -1,6 +1,18 @@
 import axios from 'axios';
 
-export default axios.create({
+const accesToken = () => localStorage.getItem( 'NC_token' );
+
+const instance = axios.create({
     baseURL: process.env.REACT_APP_BASE_URL,
-    headers: [{ 'content-type': 'application/json' }]
+    headers: [
+        { 'content-type': 'application/json' }
+    ]
 });
+
+instance.interceptors.request.use( config => {
+    if ( accesToken() )
+        config.headers.authorization = accesToken();
+    return config;
+}, error => Promise.reject( error ) );
+
+export default instance;
