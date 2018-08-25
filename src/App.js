f5bd7f8b3770ceb5589fa2cd12d3736e88eaa2eb
Brandon Villa <bornofos@gmail.com>
2018-08-24 19:29:51 -0500

    User roles added as constants and used

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import { Switch, Route, BrowserRouter } from 'react-router-dom';
 import Dashboard from './containers/Dashboard';
 import Login from './containers/Login';
 import Signin from './containers/Signin';
+import { ADMINISTRATOR, CUSTOMER } from './shared/user-roles';
 
 class App extends Component {
   render() {
@@ -15,8 +16,8 @@ class App extends Component {
       <BrowserRouter>
         <Switch>
           <Route path="/dashboard" component={Dashboard} />
-          <Route path="/admin-login" render={() => <Login type="ADMINISTRATOR" header="Inicia Sesión como administrador"/> }/>
-          <Route path="/login" render={() => <Login type="CUSTOMER" header="Inicia Sesión"/> }/>
+          <Route path="/admin-login" render={() => <Login type={ADMINISTRATOR} header="Inicia Sesión como administrador"/> }/>
+          <Route path="/login" render={() => <Login type={CUSTOMER} header="Inicia Sesión"/> }/>
           <Route path="/signin" component={Signin} />
         </Switch>
       </BrowserRouter>
---
+++ src/shared/user-roles.js
@@ -0,0 +1,2 @@
+export const ADMINISTRATOR = 'ADMINISTRATOR';
+export const CUSTOMER = 'CUSTOMER';
