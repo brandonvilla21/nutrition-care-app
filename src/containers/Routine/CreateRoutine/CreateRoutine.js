322018ca047c43853ba65296466b591d0199b851
Brandon Villa <bornofos@gmail.com>
2018-09-13 22:14:08 -0500

    Code refactor

--- src/containers/Routine/CreateRoutine/CreateRoutine.js
+++ src/containers/Routine/CreateRoutine/CreateRoutine.js
@@ -1,13 +1,6 @@
 import React, { Component } from 'react';
 import TabsRoutine from '../../../components/Routine/TabsRoutine/TabsRoutine';
 
-class CreateRoutine extends Component {
-
-  render() {
-    return (
-        <TabsRoutine />
-    );
-  }
-}
+const CreateRoutine = () => <TabsRoutine />
 
 export default CreateRoutine;
