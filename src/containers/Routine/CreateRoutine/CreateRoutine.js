bdc1dba0bfbc3ca8e18d1dafb795ce4359be57dd
Brandon Villa <bornofos@gmail.com>
2018-09-17 20:29:07 -0500

    eslint rules applied

--- src/containers/Routine/CreateRoutine/CreateRoutine.js
+++ src/containers/Routine/CreateRoutine/CreateRoutine.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import TabsRoutine from '../../../components/Routine/TabsRoutine/TabsRoutine';
 
-const CreateRoutine = () => <TabsRoutine />
+const CreateRoutine = () => <TabsRoutine />;
 
 export default CreateRoutine;
