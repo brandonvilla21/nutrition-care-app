c9d261e9a9929136205e80d953869b05d223d0a3
Brandon Villa <bornofos@gmail.com>
2018-09-18 20:51:07 -0500

    lint and server scripts added

--- server.js
+++ server.js
@@ -1,12 +1,26 @@
 const express = require( 'express' ),
       path    = require( 'path' ),
-      port    = process.env.PORT || 3000;
+      port    = process.env.PORT || 8080;
 
 const app = express();
+
+// TODO: check this method
+// const forceSSL = () => {
+//     return ( req, res, next ) => {
+//         if ( req.headers['x-forwarded-proto'] !== 'https' ) {
+//             return res.redirect(
+//                 ['https://', req.get( 'Host' ), req.url].join( '' )
+//             );
+//         }
+//         next();s
+//     };
+// };
+  
+// app.use( forceSSL() );
 
 app.use( express.static( __dirname + '/build' ) );
 
 app.get( '*', ( req, res ) =>
     res.sendFile( path.resolve( __dirname + '/build/index.html' ) ) );
 
-app.listen( port, () => console.log( `Server app running on port ${port}` ) );
+app.listen( port );
